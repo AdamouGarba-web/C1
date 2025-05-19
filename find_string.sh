7e2e13d6ed751164a7fb91eddd426443cb11a709
AdamouGarba-web <adamougarbar@gmail.com>
2025-05-19 01:10:15 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
 # Vérifie qu'on a bien deux arguments : le fichier et la chaîne
 if [ $# -ne 2 ]; then
   echo "Usage: $0 nom_du_fichier chaîne_à_rechercher"
   exit 1
 fi
-
 fichier="$1"
 chaine="$2"
-
 # Vérifie si le fichier existe
 if [ ! -f "$fichier" ]; then
   echo "Erreur : le fichier '$fichier' n'existe pas."
   exit 1
 fi
-
 # Recherche la chaîne avec grep
 if grep -q "$chaine" "$fichier"; then
   echo "La chaîne '$chaine' a été trouvée dans $fichier."
