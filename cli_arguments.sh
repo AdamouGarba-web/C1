566de1bcd936d5b9ea5ddcfb2c1c78d0560b8372
AdamouGarba-web <adamougarbar@gmail.com>
2025-05-30 16:41:13 +0100

    Delete cli_arguments.sh

--- cli_arguments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ce script affiche chaque argument reçu en ligne de commande
-
-index=1
-while [ "$#" -gt 0 ]; do
-    echo "Argument $index: $1"
-    shift
-    index=$((index + 1))
-done
